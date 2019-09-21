19c7bd1cb1ad0adb5af00e69b3de966abb015a4d
GorshkovTimur <gorshkoff.timur@gmail.com>
2019-09-21 16:06:16 +0300

    Lesson 6 HW done. Have realized RecyclerView with mankind end model)

--- app/src/main/java/com/timmyg/instaclient/ScrollingActivity.java
+++ app/src/main/java/com/timmyg/instaclient/ScrollingActivity.java
@@ -7,14 +7,22 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ScrollingActivity extends AppCompatActivity {
+
+    private List<HumanityEndModel> model;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_scrolling);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -27,5 +35,35 @@ public class ScrollingActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        initModel();
+
+        RecyclerView rv = findViewById(R.id.recycler_view);
+        rv.setLayoutManager(new GridLayoutManager(this,2));
+        ScrollingAdapter scrollingAdapter = new ScrollingAdapter(this, model);
+        rv.setAdapter(scrollingAdapter);
+
+    }
+
+
+
+    private void initModel() {
+        model = new ArrayList<>();
+        for (int i=0; i<100; i++){
+            switch (i%4) {
+                case (0):
+                    model.add(new HumanityEndModel(R.drawable.biohazard));
+                    break;
+                case (1):
+                    model.add(new HumanityEndModel(R.drawable.meteor));
+                    break;
+                case (2):
+                    model.add(new HumanityEndModel(R.drawable.nuclear));
+                    break;
+                case (3):
+                    model.add(new HumanityEndModel(R.drawable.war_machine));
+                    break;
+            }
+        }
     }
 }
